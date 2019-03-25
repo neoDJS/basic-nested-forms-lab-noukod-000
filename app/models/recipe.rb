3aba95b8d5c5562d6ee72dde2b4bd6cb41b683b6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-25 03:38:48 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
+  accepts_nested_attributes_for :ingredients
 
   # def ingredient_attributes=(ingred)
   #   self.ingredient = Ingredient.find_or_create_by(name: ingred.name)
