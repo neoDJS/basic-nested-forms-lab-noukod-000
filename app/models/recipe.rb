56a5dec5ae9eb442454bc08c8518ddac8290613a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-25 03:49:58 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  # def ingredient_attributes=(ingred)
+  #   self.ingredient = Ingredient.find_or_create_by(name: ingred.name)
+  #   self.ingredients.update(artist)
+  # end
 end
